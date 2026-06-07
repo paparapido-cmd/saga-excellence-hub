82237bab35546f5e6afd6b0fe0106065cb004f89
paparapido-cmd <paparapido@gmail.com>
2026-06-07 23:11:58 +0200

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -38,7 +38,7 @@ export default function Contact() {
     try {
       const validatedData = contactSchema.parse(formData);
 
-      const response = await fetch("https://formspree.io/f/xpwrknqb", {
+      const response = await fetch("https://formspree.io/f/maqzeggw", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
